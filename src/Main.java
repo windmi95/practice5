1cba1ec00f22ccf7f2bd5f64465f199332c65296
LMH <windmi95@nate.com>
2023-06-02 17:26:17 +0900

    pareint

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        /* String num = "123456";
+        int n = Integer.parseInt(num);
+        System.out.println(n);
+        */
+        int n = 123;
+        String num = "" + n;
+        System.out.println(num);
+    }
+}
